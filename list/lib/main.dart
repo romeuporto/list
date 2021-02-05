af89c9999c0d1296eea77c17f8b7baaf7ebc9d9e
Romeu Porto <romeu.porto.sms@gmail.com>
2021-02-04 21:59:14 -0300

    acrescentado lista

--- list/lib/main.dart
+++ list/lib/main.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/scheduler.dart';
 import 'package:list/Home.dart';
 
 void main (){
-  debugPrintBeginFrameBanner: false;
   runApp(MaterialApp(
     home: Home(),
   ));
