b4ea1f9107da9ef9acf41e5b9a2538f060d097f1
Romeu Porto <romeu.porto.sms@gmail.com>
2021-02-04 21:39:21 -0300

    page home

---
+++ list/lib/Home.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Lista"),
+      ),
+      body: Container(
+        padding: EdgeInsets.all(20),
+        child: ,
+      )
+    );
+  }
+}
+
--- list/lib/main.dart
+++ list/lib/main.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/scheduler.dart';
+import 'package:list/Home.dart';
 
 void main (){
+  debugPrintBeginFrameBanner: false;
   runApp(MaterialApp(
-    home: ,
+    home: Home(),
   ));
 }
+
